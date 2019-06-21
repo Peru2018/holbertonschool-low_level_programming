355e4016f3a09d9941ebb2e24c3487b1102debcc
Alexis Rodriguez Fernandez <alexisrodriguez1093@gmail.com>
2019-06-21 03:05:46 +0000

    Add answer for task 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	i = 48;
+
+	while (i <= 57)
+{
+	putchar(i);
+
+
+	i++;
+}
+
+	putchar('\n');
+return (0);
+}
