3602719c3c4650615ea2d248a61ac075dbdda2f6
Alexis Rodriguez Fernandez <alexisrodriguez1093@gmail.com>
2019-06-25 23:26:34 +0000

    Add answer for taask 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *  _isalpha - check the code for Holberton School students.
+ * @c: lowercase variable
+ *
+ * Return: int.
+ */
+int _isalpha(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		if (c >= 'A' && c <= 'Z')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+}
