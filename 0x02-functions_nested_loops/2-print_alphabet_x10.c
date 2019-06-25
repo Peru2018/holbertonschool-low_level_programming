93adfdb0b8cb0cb90c24d3f9e50f710b4b93822d
Alexis Rodriguez Fernandez <alexisrodriguez1093@gmail.com>
2019-06-25 03:52:02 +0000

    Add answer for task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - check the code for Holberton School students.
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+	char Alphabet;
+	int counter;
+
+	counter = 1;
+	while (counter <= 10)
+	{
+		Alphabet = 'a';
+		while (Alphabet <= 'z')
+		{
+			_putchar (Alphabet);
+			Alphabet++;
+		}
+	_putchar ('\n');
+	counter++;
+	}
+}
+
