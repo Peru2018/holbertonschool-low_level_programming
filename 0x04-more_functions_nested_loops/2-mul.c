a0bf539aeee55cb5583c8fa0a03f79ff10d18e0a
Alexis Rodriguez Fernandez <alexisrodriguez1093@gmail.com>
2019-06-27 18:33:42 +0000

    Add answer for task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - write a function that multiplies two integers.
+ * @a: integer1
+ * @b: integer2
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+
+return (a * b);
+}
