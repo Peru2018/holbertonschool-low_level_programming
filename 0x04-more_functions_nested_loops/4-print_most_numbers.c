29f512a4191d140d9faae0abf62ce4c24ea87738
Alexis Rodriguez Fernandez <alexisrodriguez1093@gmail.com>
2019-06-27 20:00:03 +0000

    Add answer for task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print the numbers 0 to 9 but do not print 2 and 4.
+ * @a: integer.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+
+		if (!(a == 2 || a == 4))
+		{
+			_putchar('0' + a);
+		}
+
+	}
+
+	_putchar('\n');
+
+}
