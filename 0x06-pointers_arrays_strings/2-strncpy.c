1486fff7d7c2cb878b4b80ac5a165f3bf169623d
Alexis Rodriguez Fernandez <alexisrodriguez1093@gmail.com>
2019-07-05 23:36:11 +0000

    Add answer for task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strncpy - write a function that copies a string.
+ * @dest: destination
+ * @src: source
+ * @n: bytes
+ *
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
+
